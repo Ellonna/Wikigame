b77a2a608cac93ec27eabfddaf1cee1be1440462
Bertille <bertille.pinchon@gmail.com>
2017-05-02 09:07:41 +0200

    Minor changes and tests in curl.php.

--- curl.php
+++ curl.php
@@ -35,24 +35,55 @@ function GetWikiHeader($url){
     //Set URL
     curl_setopt($ch, CURLOPT_URL, $url);
     //Return page
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_NOBODY, 1);
     //Send User-agent
     curl_setopt($ch, CURLOPT_USERAGENT, 'Nous ne sommes pas malicieux');
     //Do not verify certificate
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     //Get header
-    curl_setopt($ch, CURLOPT_HEADER, true);
-    //curl_setopt($ch, CURLOPT_HEADERFUNCTION, "HandleHeaderLine");
+    //curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_HEADERFUNCTION, "HandleHeaderLine");
     //Execute request and stock the result into a variable
-    $resultat = curl_exec($ch);
+    $headers = curl_exec($ch);
+    var_dump($headers);
     //Close cURL connection
     curl_close($ch);
     //Return variable
-    return $resultat;
+    //list($headers, $response) = explode("\r\n\r\n", $resultat, 2);
+    // $headers now has a string of the HTTP headers
+    // $response is the body of the HTTP response
+
+    /*$headers = explode("\n", $headers);
+    var_dump ($headers);
+    foreach($headers as $header) {
+        if (stripos($header, 'Location:') !== false) {
+            echo "The location header is: '$header'";
+        }
+    }
+    return $resultat;*/
+    function get_headers_from_curl_response($response)
+    {
+    $headers = array();
+
+    $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+
+    foreach (explode("\r\n", $header_text) as $i => $line)
+        if ($i === 0)
+            $headers['http_code'] = $line;
+        else
+        {
+            list ($key, $value) = explode(': ', $line);
+
+            $headers[$key] = $value;
+        }
+
+    return $headers;
+    }
+    $headersTab = get_headers_from_curl_response($headers);
+    var_dump($headersTab);
+    return $headersTab;
 }
-
-
 
 function GetArticle($page){
     //Create new DOMDocument
@@ -70,9 +101,3 @@ function GetArticle($page){
     } 
     return $innerHTML;
 }
-
-function GetPageHeader($url){
-    $headers=get_headers($host, 1);
-    return $headers;
-}
-
