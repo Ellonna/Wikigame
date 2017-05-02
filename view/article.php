a202c39f647e521396ca740f88ac6314cadd668d
Bertille <bertille.pinchon@gmail.com>
2017-05-02 17:17:23 +0200

    Reorganized index.php, cut it into several elements of ./view.

---
+++ view/article.php
@@ -0,0 +1,12 @@
+<br/>
+Ce projet est jeune et dynamique ! ♥
+</br>
+<img id="macron" src="https://image.noelshack.com/fichiers/2017/02/1484399870-macron.png" />
+<?php
+    //$page = GetWikipage($wikipediaURL);
+    //$head = GetPageHeader($wikipdiaURL);
+    $testpage = GetWikiPage('https://fr.wikipedia.org/wiki/CURL');
+    GetArticle($testpage);
+    //GetWikiHeader('$wikipediaRandURL');
+    //echo bite;
+?>
