a9db3592986475fb0714ba83ee109fd3e3b08547
Phalao <canalbloglouissama@gmail.com>
2017-05-03 22:31:54 +0200

    Mise en place du deuxième boutton pour le refresh + tentative de php pour modifier la variable $GameState

--- view/StartPage.php
+++ view/StartPage.php
@@ -1,4 +1,21 @@
  <form>
-     <input type="submit" value="Start the fucking game!">
-      <form action="index.php">
+     //Bouton pour lancer le jeu
+     <input type="submit" value="Start the fucking game!" name="gamelcher">
+
+     <?php
+        if(<input type="submit" value="envoyer" name="gamelcher"/>){ //Détecte quand on appuye sur submit
+            $GameState=1;
+        }
+     </?>
+            <form action="index.php"></form>//là j'ai un peu de mal a voir où je dois la cassé cette ligne(toute à la fin,avant ou après chaque bouttons, dans mon cul...)
+
+     //Boutton pour refresh
+     <input type="submit" value="refresh">
+
+     <?php
+        if(<input type="submit" value="refresh" name="gamerestart"/>){ //Détecte quand on appuye sur submit
+            $GameState=0;
+        }
+     </?>
+
 </form>
