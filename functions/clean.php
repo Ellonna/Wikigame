a4c4499553f11cb7314acfe5516ee4f3d95b9270
Phalao <canalbloglouissama@gmail.com>
2017-05-04 13:18:23 +0200

    J'ai supprimé ça car Bertille elle me l'a demandé

--- functions/clean.php
+++
@@ -1,9 +0,0 @@
-<?php
-function clean(){
-    //on efface le début du lien pour ne garder que le titre de l'article
-    $result = preg_replace("/https:\/\/fr.wikipedia.ord\/wiki\//","",$result);
-    //On remplace les %2C ect par les bons caracteres
-    $result=preg_replace("/_/","_",$result);
-
-    return $result;//On rencoie le titre
-}
